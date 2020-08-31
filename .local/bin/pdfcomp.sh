8ce6ff713cc7c20abb033e85a5c750c707a8fff0
MaW <maw@aaathats3as.com>
2020-08-31 19:54:36 +0200

    Update pdfcomp script

--- .local/bin/pdfcomp.sh
+++ .local/bin/pdfcomp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 inputFile=$1
-outputFile=$2
 
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dBATCH -sOutputFile="$outputFile" "$inputFile"
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dBATCH -sOutputFile="$(basename $inputFile .pdf).pdf_comp" "$inputFile" && rm $inputFile && mv "$(basename $inputFile .pdf).pdf_comp"  $inputFile
